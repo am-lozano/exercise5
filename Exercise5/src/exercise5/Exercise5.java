10eaa0bbc1a29e34b7253ea656921d42d30014ed
am.lozano <am.lozano@106-18.info2>
2024-09-23 14:02:23 +0200

    Exercise5

---
+++ Exercise5/src/exercise5/Exercise5.java
@@ -0,0 +1,12 @@
+package exercise5;
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		System.out.println("Antonio Manuel");
+		System.out.println("C/Badajoz");
+		System.out.println("655 24 34 47");
+
+	}
+
+}
